42cb922f37b429c8a6d452036df7dfa5e265b150
Christina Symon <christinasymon@Christinas-MacBook-Pro.local>
2018-09-05 17:55:23 -0700

    modified with shader file

--- PA1/src/shader.cpp
+++ PA1/src/shader.cpp
@@ -1,5 +1,5 @@
 #include "shader.h"
-
+#include <fstream>
 Shader::Shader()
 {
   m_shaderProg = 0;
@@ -39,39 +39,28 @@ bool Shader::AddShader(GLenum ShaderType)
 
   if(ShaderType == GL_VERTEX_SHADER)
   {
-    s = "#version 330\n \
-          \
-          layout (location = 0) in vec3 v_position; \
-          layout (location = 1) in vec3 v_color; \
-          \
-          smooth out vec3 color; \
-          \
-          uniform mat4 projectionMatrix; \
-          uniform mat4 viewMatrix; \
-          uniform mat4 modelMatrix; \
-          \
-          void main(void) \
-          { \
-            vec4 v = vec4(v_position, 1.0); \
-            gl_Position = (projectionMatrix * viewMatrix * modelMatrix) * v; \
-            color = v_color; \
-          } \
-          ";
+	std::ifstream ifs;
+	std::string temp;
+	ifs.open("pa1_shaders.vert");
+	while(getline(ifs,temp))
+	{
+		s.append(temp);
+		s.append("\n");
+	}
+	ifs.close();
   }
   else if(ShaderType == GL_FRAGMENT_SHADER)
   {
-    s = "#version 330\n \
-          \
-          smooth in vec3 color; \
-          \
-          out vec4 frag_color; \
-          \
-          void main(void) \
-          { \
-             frag_color = vec4(color.rgb, 1.0); \
-          } \
-          ";
-  }
+    std::ifstream ifs;
+	std::string temp;
+	ifs.open("pa1_shaders.frag");
+	while(getline(ifs,temp))
+	{
+		s.append(temp);
+		s.append("\n");
+	}
+	ifs.close();
+ }
 
   GLuint ShaderObj = glCreateShader(ShaderType);
 
